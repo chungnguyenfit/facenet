1f6181761c318a738ce281a525bab5e0a7af71f2
Thien Nguyen <tnguyen@patsnap.com>
2019-01-17 15:56:26 +0800

    test mobilenet v2

---
+++ src/models/mobilenet_v2.py
@@ -0,0 +1,35 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+import tensorflow_hub as hub
+
+
+def inference(images, keep_probability, phase_train=True, bottleneck_layer_size=128, weight_decay=0.0, reuse=None):
+    batch_norm_params = {
+        # Decay for the moving averages.
+        'decay': 0.995,
+        # epsilon to prevent 0s in variance.
+        'epsilon': 0.001,
+        # force in-place updates of mean and variance estimates
+        'updates_collections': None,
+        # Moving averages ends up in the trainable variables collection
+        'variables_collections': [ tf.GraphKeys.TRAINABLE_VARIABLES ],
+    }
+    with slim.arg_scope([slim.conv2d, slim.fully_connected],
+                        weights_initializer=slim.xavier_initializer_conv2d(uniform=True),
+                        weights_regularizer=slim.l2_regularizer(weight_decay),
+                        normalizer_fn=slim.batch_norm,
+                        normalizer_params=batch_norm_params):
+        with tf.variable_scope('mobilenet_v2', [images], reuse=reuse):
+            with slim.arg_scope([slim.batch_norm, slim.dropout],
+                                is_training=phase_train):
+                #saver = tf.train.import_meta_graph('../model/backbone/mobilenet_v2_1.4_224.ckpt.meta')
+                #saver.restore(sess,tf.train.latest_checkpoint('./'))
+                module = hub.Module("https://tfhub.dev/google/imagenet/mobilenet_v2_140_224/feature_vector/2")
+                height, width = hub.get_expected_image_size(module)
+                features = module(images)  # Features with shape [batch_size, num_features].
+                
+    return features, None
